36f314828554181c56cc1c4c4a82163cfb7891a2
Personal tientran6110 <tientran6110@gmail.com>
2024-11-01 21:17:58 -0700

    ESLint and Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 
   return (
     <main>
-      <h1 className='font-bold text-3xl'>Welcome to Next.js</h1>
+      <h1 className='text-3xl font-bold'>Welcome to Next.js</h1>
       <Hello />
     </main>
   );
